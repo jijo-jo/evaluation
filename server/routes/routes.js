1b46abf801019f64b33839092060a5e57b36cbe8
Jijo Joseph <75742757+jijo12@users.noreply.github.com>
2021-11-27 17:36:45 +0530

    Update routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 const assetRoutes = require('./asset.route');
 
-router.use('/assets', staffRoutes);
+router.use('/assets', assetRoutes);
 module.exports = router;
